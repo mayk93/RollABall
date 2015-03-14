e8c5a35b681d0148c7cb71f926117aa98a97121f
mayk93 <mihai.mandrescu@gmail.com>
2015-03-14 09:40:58 +0200

    Roll a Ball
    Simple roll a ball tutorial

---
+++ PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 1.0f;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    // Before phisycs calculations
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement*speed*Time.deltaTime);
+    }
+}
